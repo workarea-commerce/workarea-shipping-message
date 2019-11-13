329585131d8aed3441b919bf1e68325ed1232d20
Ben Crouse <bcrouse@workarea.com>
2019-11-13 14:05:45 -0500

    Upgrade for v3.5 compatibility

--- config/initializers/appends.rb
+++ config/initializers/appends.rb
@@ -1,5 +1,10 @@
 module Workarea
   Plugin.append_partials(
+    'admin.discount_cards',
+    'workarea/admin/pricing_discounts/message_card'
+  )
+
+  Plugin.append_partials(
     "storefront.cart_checkout_actions",
     "workarea/storefront/carts/shipping_message"
   )
