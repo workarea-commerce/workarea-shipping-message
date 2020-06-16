15c528be4de5b91f6b9f69d4dc287804be65c00d
Ben Crouse <bcrouse@workarea.com>
2020-06-16 14:37:47 -0400

    Scope routes to include locale

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,13 @@
 Workarea::Admin::Engine.routes.draw do
-  namespace :shipping_message do
-    resource :settings, only: [:edit, :update]
-  end
+  scope '(:locale)', constraints: Workarea::I18n.routes_constraint do
+    namespace :shipping_message do
+      resource :settings, only: [:edit, :update]
+    end
 
-  resources :pricing_discounts, except: [:new, :create] do
-    member do
-      get :message
+    resources :pricing_discounts, except: [:new, :create] do
+      member do
+        get :message
+      end
     end
   end
 end
